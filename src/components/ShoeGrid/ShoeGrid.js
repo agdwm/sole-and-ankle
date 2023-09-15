61c688af8b048313a8d97d988e4e4ed7634c6382
almudenaredradix <almudena@redradix.com>
2023-09-15 14:06:19 +0200

    :lipstick: Finish the layout of the ShowGrid component

--- src/components/ShoeGrid/ShoeGrid.js
+++ src/components/ShoeGrid/ShoeGrid.js
@@ -8,12 +8,24 @@ const ShoeGrid = () => {
   return (
     <Wrapper>
       {SHOES.map((shoe) => (
-        <ShoeCard key={shoe.slug} {...shoe} />
+        <ShoeWrapper key={shoe.slug}>
+          <ShoeCard {...shoe} />
+        </ShoeWrapper>
       ))}
     </Wrapper>
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 32px;
+`;
+
+const ShoeWrapper = styled.div`
+  /* 'width' is a suggestion, meanwhile 'min-width' is a hard requirement */
+  min-width: 275px;
+  flex: 1;
+`;
 
 export default ShoeGrid;
